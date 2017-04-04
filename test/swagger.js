63904aca445f9ed6982211357f6362dc18b9c3d5
delvedor <tommydelved@gmail.com>
2017-04-04 20:44:39 +0200

    Updated test

--- test/swagger.js
+++ test/swagger.js
@@ -4,12 +4,25 @@ const t = require('tap')
 const test = t.test
 const Fastify = require('fastify')
 const Swagger = require('swagger-parser')
+const fastifySwagger = require('../index')
+
+const swaggerInfo = {
+  swagger: {
+    info: {
+      title: 'Test swagger',
+      description: 'testing the fastify swagger api',
+      version: '0.1.0'
+    },
+    host: 'localhost',
+    schemes: ['http']
+  }
+}
 
 test('fastify.swagger should exist', t => {
   t.plan(2)
   const fastify = Fastify()
 
-  fastify.register(require('./index'))
+  fastify.register(fastifySwagger)
 
   fastify.ready(err => {
     t.error(err)
@@ -21,17 +34,7 @@ test('fastify.swagger should return a valid swagger file', t => {
   t.plan(2)
   const fastify = Fastify()
 
-  fastify.register(require('./index'), {
-    swagger: {
-      info: {
-        title: 'Test swagger',
-        description: 'testing the fastify swagger api',
-        version: '0.1.0'
-      },
-      host: 'localhost',
-      schemes: ['http']
-    }
-  })
+  fastify.register(fastifySwagger, swaggerInfo)
 
   const opts1 = {
     out: {
@@ -94,5 +97,44 @@ test('fastify.swagger should return a valid swagger file', t => {
       .catch(function (err) {
         t.fail(err)
       })
+  })
+})
+
+test('fastify.swagger basic properties', t => {
+  t.plan(6)
+  const fastify = Fastify()
+
+  fastify.register(fastifySwagger, swaggerInfo)
+
+  const opts = {
+    payload: {
+      type: 'object',
+      properties: {
+        hello: { type: 'string' },
+        obj: {
+          type: 'object',
+          properties: {
+            some: { type: 'string' }
+          }
+        }
+      }
+    }
+  }
+
+  fastify.get('/', opts, () => {})
+
+  fastify.ready(err => {
+    t.error(err)
+
+    const swaggerObject = fastify.swagger({
+      json: true,
+      return: true
+    })
+
+    t.equal(swaggerObject.info, swaggerInfo.swagger.info)
+    t.equal(swaggerObject.host, swaggerInfo.swagger.host)
+    t.equal(swaggerObject.schemes, swaggerInfo.swagger.schemes)
+    t.ok(swaggerObject.paths)
+    t.ok(swaggerObject.paths['/'])
   })
 })
