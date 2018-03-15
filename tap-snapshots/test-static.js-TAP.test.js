8ae17e5428786a8a12636c7658927eb759a67fc5
Pavel <me@pavelpolyakov.com>
2018-03-15 11:27:13 +0100

    introducing static mode for the fastify-swagger (#30)

---
+++ tap-snapshots/test-static.js-TAP.test.js
@@ -0,0 +1,52 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/static.js TAP specification validation check works > undefined 1`] = `
+Error: specification is missing in the module options
+`
+
+exports[`test/static.js TAP specification validation check works > undefined 2`] = `
+Error: specification is not an object
+`
+
+exports[`test/static.js TAP specification validation check works > undefined 3`] = `
+Error: specification.path is missing, should be path to the file
+`
+
+exports[`test/static.js TAP specification validation check works > undefined 4`] = `
+Error: specification.path is not a string
+`
+
+exports[`test/static.js TAP specification validation check works > undefined 5`] = `
+Error: /hello/lionel.richie does not exist
+`
+
+exports[`test/static.js TAP swagger route returns json > undefined 1`] = `
+{ openapi: '3.0.0',
+  info: 
+   { description: 'Test swagger specification',
+     version: '1.0.0',
+     title: 'Test swagger specification',
+     contact: { email: 'super.developer@gmail.com' } },
+  servers: 
+   [ { url: 'http://localhost:3000/',
+       description: 'Localhost (uses test data)' } ],
+  paths: 
+   { '/status': 
+      { get: 
+         { description: 'Status route, so we can check if server is alive',
+           tags: [ 'Status' ],
+           responses: 
+            { '200': 
+               { description: 'Server is alive',
+                 content: 
+                  { 'application/json': 
+                     { schema: 
+                        { type: 'object',
+                          properties: { health: { type: 'boolean' }, date: { type: 'string' } },
+                          example: { health: true, date: '2018-02-19T15:36:46.758Z' } } } } } } } } } }
+`
