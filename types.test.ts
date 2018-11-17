9641f1380268935cf5500362026997b0ce1841a2
Ricardo Devis Agullo <mkredz@gmail.com>
2018-11-17 21:45:10 +0100

    add TS types

---
+++ types.test.ts
@@ -0,0 +1,47 @@
+import fastify = require('fastify');
+import fastifySwagger = require('../fastify-swagger');
+
+const app = fastify();
+
+app.register(fastifySwagger);
+app.register(fastifySwagger, {});
+app.register(fastifySwagger, {
+  mode: 'static',
+  specification: {
+    document: 'path'
+  }
+});
+
+app
+  .register(fastifySwagger, {
+    routePrefix: '/documentation',
+    swagger: {
+      info: {
+        title: 'Test swagger',
+        description: 'testing the fastify swagger api',
+        version: '0.1.0'
+      },
+      externalDocs: {
+        url: 'https://swagger.io',
+        description: 'Find more info here'
+      },
+      host: 'localhost',
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json'],
+      tags: [
+        { name: 'user', description: 'User related end-points' },
+        { name: 'code', description: 'Code related end-points' }
+      ],
+      securityDefinitions: {
+        apiKey: {
+          type: 'apiKey',
+          name: 'apiKey',
+          in: 'header'
+        }
+      }
+    }
+  })
+  .ready(err => {
+    app.swagger();
+  });
