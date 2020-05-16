adf995ebef9ad6c237fa8e1d0844717d1906ecf8
Manuel Spigolon <behemoth89@gmail.com>
2020-05-16 19:10:11 +0200

    wip to resolving

--- test/json-schema.test.js
+++ test/json-schema.test.js
@@ -6,28 +6,60 @@ const Fastify = require('fastify')
 const fastifySwagger = require('../index')
 // const Swagger = require('swagger-parser')
 
-test('support $ref schema', t => {
+test('support $ref schema', async t => {
   t.plan(1)
 
   const fastify = Fastify()
-  fastify.register(fastifySwagger, { exposeRoute: true })
-
   fastify.addSchema({
-    $id: 'http://example.com/',
+    $id: 'example',
     type: 'object',
     properties: {
       hello: { type: 'string' }
     }
   })
 
-  fastify.post('/', {
-    handler () {},
-    schema: {
-      body: { $ref: 'http://example.com#/properties/hello' }
-    }
+  fastify.register(fastifySwagger, {
+    routePrefix: '/docs',
+    exposeRoute: true
   })
 
-  fastify.ready(err => {
-    t.error(err)
+  fastify.register((instance, opts, next) => {
+    instance.addSchema({
+      $id: 'subschema-two',
+      type: 'object',
+      properties: {
+        hello: { type: 'string' }
+      }
+    })
+
+    instance.register((subinstance, opts, next) => {
+      subinstance.addSchema({
+        $id: 'subschema-three',
+        type: 'object',
+        properties: {
+          hello: { type: 'string' }
+        }
+      })
+
+      subinstance.post('/', {
+        handler () {},
+        schema: {
+          body: { $id: 'bug', $ref: 'example#/properties/hello' }
+          // TODO
+          // querystring: { $ref: 'subschema-two#/properties/hello' },
+          // params: { $ref: 'subschema-two#/properties/hello' },
+          // headers: { $ref: 'subschema-three#/properties/hello' }
+        }
+      })
+
+      next()
+    })
+
+    next()
   })
+
+  await fastify.ready() // ? inject ignore ready?!
+  const res = await fastify.inject('/docs/json')
+  t.pass('done')
+  require('fs').writeFileSync('./out.json', JSON.stringify(res.json(), null, 2))
 })
