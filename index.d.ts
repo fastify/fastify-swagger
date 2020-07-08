fastify/fastify-swagger

commit 530514a394c58fa8d26b5084a813de2b49a42d17

update fastify to v3

index.d.ts
@@ -1,54 +1,46 @@
-import * as http from 'http';
-import * as fastify from 'fastify';
+import { FastifyPlugin } from 'fastify';
 import * as SwaggerSchema from 'swagger-schema-official';
-import * as http2 from 'http2';
 
-declare namespace fastifySwagger {
-  interface FastifySwaggerOptions {
-    mode?: 'static' | 'dynamic';
-    /**
-     * Overwrite the swagger url end-point
-     * @default /documentation
-     */
-    routePrefix?: string;
-    /**
-     * To expose the documentation api
-     * @default false
-     */
-    exposeRoute?: boolean;
-  }
+interface FastifySwaggerOptions {
+  mode?: 'static' | 'dynamic';
+  /**
+   * Overwrite the swagger url end-point
+   * @default /documentation
+   */
+  routePrefix?: string;
+  /**
+   * To expose the documentation api
+   * @default false
+   */
+  exposeRoute?: boolean;
+}
 
-  interface FastifyDynamicSwaggerOptions extends FastifySwaggerOptions {
-    mode?: 'dynamic';
-    swagger?: Partial<SwaggerSchema.Spec>;
-    /**
-     * Overwrite the route schema
-     */
-    transform?: Function;
-  }
+interface FastifyDynamicSwaggerOptions extends FastifySwaggerOptions {
+  mode?: 'dynamic';
+  swagger?: Partial<SwaggerSchema.Spec>;
+  /**
+   * Overwrite the route schema
+   */
+  transform?: Function;
+}
 
-  interface StaticPathSpec {
-    path: string;
-    postProcessor?: (spec: SwaggerSchema.Spec) => SwaggerSchema.Spec;
-    baseDir: string;
-  }
+interface StaticPathSpec {
+  path: string;
+  postProcessor?: (spec: SwaggerSchema.Spec) => SwaggerSchema.Spec;
+  baseDir: string;
+}
 
-  interface StaticDocumentSpec {
-    document: string;
-  }
+interface StaticDocumentSpec {
+  document: string;
+}
 
-  interface FastifyStaticSwaggerOptions extends FastifySwaggerOptions {
-    mode: 'static';
-    specification: StaticPathSpec | StaticDocumentSpec;
-  }
+interface FastifyStaticSwaggerOptions extends FastifySwaggerOptions {
+  mode: 'static';
+  specification: StaticPathSpec | StaticDocumentSpec;
 }
 
 declare module 'fastify' {
-  interface FastifyInstance<
-    HttpServer,
-    HttpRequest,
-    HttpResponse
-  > {
+  interface FastifyInstance {
     swagger: (
       opts?: {
         yaml?: boolean;
@@ -56,7 +48,7 @@ declare module 'fastify' {
     ) => SwaggerSchema.Spec;
   }
 
-  interface RouteSchema {
+  interface FastifySchema {
     hide?: boolean;
     tags?: string[];
     description?: string;
@@ -66,14 +58,8 @@ declare module 'fastify' {
   }
 }
 
-declare function fastifySwagger<
-  HttpServer extends (http.Server | http2.Http2Server),
-  HttpRequest extends (http.IncomingMessage | http2.Http2ServerRequest),
-  HttpResponse extends (http.ServerResponse | http2.Http2ServerResponse),
-  SwaggerOptions = (fastifySwagger.FastifyStaticSwaggerOptions | fastifySwagger.FastifyDynamicSwaggerOptions)
->(
-  fastify : fastify.FastifyInstance<HttpServer, HttpRequest, HttpResponse>, 
-  opts : SwaggerOptions
-) : void;
+type SwaggerOptions = (FastifyStaticSwaggerOptions | FastifyDynamicSwaggerOptions)
 
-export = fastifySwagger;
+declare const fastifySwagger: FastifyPlugin<SwaggerOptions>
+
+export default fastifySwagger;

test/types/http2-types.test.ts
@@ -1,7 +1,9 @@
-import fastify = require('fastify');
-import fastifySwagger = require('../fastify-swagger');
+import fastify from 'fastify';
+import fastifySwagger from '../..';
 
-const app = fastify();
+const app = fastify({
+  http2: true
+});
 
 app.register(fastifySwagger);
 app.register(fastifySwagger, {});
@@ -16,22 +18,21 @@ app.register(fastifySwagger, {
 });
 
 app.put('/some-route/:id', {
-    schema: {
-      description: 'put me some data',
-      tags: ['user', 'code'],
-      summary: 'qwerty',
-      consumes: ['application/json', 'multipart/form-data'],
-      security: [{ apiKey: []}]
-    }
-  }, (req, reply) => {});
+  schema: {
+    description: 'put me some data',
+    tags: ['user', 'code'],
+    summary: 'qwerty',
+    security: [{ apiKey: []}]
+  }
+}, (req, reply) => {});
 
 app.get('/public/route', {
-    schema: {
-      description: 'returns 200 OK',
-      summary: 'qwerty',
-      security: []
-    }
-  }, (req, reply) => {});
+  schema: {
+    description: 'returns 200 OK',
+    summary: 'qwerty',
+    security: []
+  }
+}, (req, reply) => {});
 
 app
   .register(fastifySwagger, {

test/types/types.test.ts
@@ -1,9 +1,7 @@
-import fastify = require('fastify');
-import fastifySwagger = require('.');
+import fastify from 'fastify';
+import fastifySwagger from '../..';
 
-const app = fastify({
-  http2: true
-});
+const app = fastify();
 
 app.register(fastifySwagger);
 app.register(fastifySwagger, {});
@@ -22,17 +20,18 @@ app.put('/some-route/:id', {
       description: 'put me some data',
       tags: ['user', 'code'],
       summary: 'qwerty',
+      consumes: ['application/json', 'multipart/form-data'],
       security: [{ apiKey: []}]
     }
   }, (req, reply) => {});
 
 app.get('/public/route', {
-  schema: {
-    description: 'returns 200 OK',
-    summary: 'qwerty',
-    security: []
-  }
-}, (req, reply) => {});
+    schema: {
+      description: 'returns 200 OK',
+      summary: 'qwerty',
+      security: []
+    }
+  }, (req, reply) => {});
 
 app
   .register(fastifySwagger, {

index.js
@@ -23,6 +23,6 @@ function fastifySwagger (fastify, opts, next) {
 }
 
 module.exports = fp(fastifySwagger, {
-  fastify: '>=2.0.0',
+  fastify: '>=3.x',
   name: 'fastify-swagger'
 })

test/transform.js
@@ -34,7 +34,7 @@ test('transform should fail with a value other than Function', t => {
 
   fastify.register(fastifySwagger, invalid)
 
-  fastify.setSchemaCompiler(schema => Joi.validate(schema))
+  fastify.setValidatorCompiler(schema => Joi.validate(schema))
   fastify.get('/example', opts, () => {})
 
   fastify.ready(err => {
@@ -48,7 +48,7 @@ test('transform should work with a Function', t => {
 
   fastify.register(fastifySwagger, valid)
 
-  fastify.setSchemaCompiler(schema => Joi.validate(schema))
+  fastify.setValidatorCompiler(schema => Joi.validate(schema))
   fastify.get('/example', opts, () => {})
 
   fastify.ready(err => {

commit e9c939151bf30adc78ce566ec9682c40f26b53a2

fix: Correct types
Partial revert of
https://github.com/fastify/fastify-swagger/pull/238/commits/530514a394c58fa8d26b5084a813de2b49a42d17
which broke the types declared in this plugin. I can't find anything in
the PR or commit message that explains why this was done, but this module does
not have a default export.

Fixes: #260

index.d.ts
@@ -62,4 +62,4 @@ type SwaggerOptions = (FastifyStaticSwaggerOptions | FastifyDynamicSwaggerOption
 
 declare const fastifySwagger: FastifyPlugin<SwaggerOptions>
 
-export default fastifySwagger;
+export = fastifySwagger;

test/types/http2-types.test.ts
@@ -1,5 +1,5 @@
 import fastify from 'fastify';
-import fastifySwagger from '../..';
+import fastifySwagger = require('../..');
 
 const app = fastify({
   http2: true

test/types/types.test.ts
@@ -1,5 +1,5 @@
 import fastify from 'fastify';
-import fastifySwagger from '../..';
+import fastifySwagger = require('../..');
 
 const app = fastify();
 