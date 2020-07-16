d83f6fb1ad2f42232603f4042c42bce04c0ee499
Maksim Sinik <maksim@sinik.it>
2020-07-16 15:21:43 +0200

    Adds also test for import/require on TS side

--- test/types/check-default-export.ts
+++ test/types/check-default-export.ts
@@ -2,5 +2,7 @@ import { expectAssignable } from 'tsd';
 
 import fastifySwagger from '../..';
 import * as fastifySwaggerNamespace from '../..';
+import fastifySwaggerRequired = require('../..')
 
 expectAssignable<typeof fastifySwagger>(fastifySwaggerNamespace);
+expectAssignable<typeof fastifySwaggerRequired>(fastifySwaggerNamespace);
