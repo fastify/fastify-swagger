e62fee5c1aba87ad6614ee2023f0a20fd7b4f7dc
Brice RUZAND <b.ruzand@nodya-group.com>
2021-09-20 09:14:58 +0200

    #462 Allow nested  to resolve for OpenAPI : failing test

--- test/spec/openapi/refs.js
+++ test/spec/openapi/refs.js
@@ -6,8 +6,8 @@ const Swagger = require('swagger-parser')
 const fastifySwagger = require('../../../index')
 const { openapiOption } = require('../../../examples/options')
 
-test('support $ref schema', t => {
-  t.plan(3)
+test('support $ref schema', (t) => {
+  t.plan(4)
   const fastify = Fastify()
 
   fastify.register(fastifySwagger, openapiOption)
@@ -17,12 +17,47 @@ test('support $ref schema', t => {
     done()
   })
 
-  fastify.ready(err => {
+  fastify.ready((err) => {
     t.error(err)
 
     const openapiObject = fastify.swagger()
     t.equal(typeof openapiObject, 'object')
+    t.match(Object.keys(openapiObject.components.schemas), ['def-0'])
 
+    Swagger.validate(openapiObject)
+      .then(function (api) {
+        t.pass('valid swagger object')
+      })
+      .catch(function (err) {
+        t.fail(err)
+      })
+  })
+})
+
+test('support nested $ref schema', (t) => {
+  t.plan(5)
+  const fastify = Fastify()
+
+  fastify.register(fastifySwagger, openapiOption)
+  fastify.register(function (instance, _, done) {
+    instance.addSchema({ $id: 'OrderItem', type: 'object', properties: { id: { type: 'integer' } } })
+    instance.addSchema({ $id: 'Order', type: 'object', properties: { products: { type: 'array', items: { $ref: 'OrderItem' } } } })
+    instance.post('/', { schema: { body: { $ref: 'Order' }, response: { 200: { $ref: 'Order' } } } }, () => {})
+    done()
+  })
+
+  fastify.ready((err) => {
+    t.error(err)
+
+    const openapiObject = fastify.swagger()
+    t.equal(typeof openapiObject, 'object')
+    t.match(Object.keys(openapiObject.components.schemas), ['def-0', 'def-1'])
+
+    // OrderItem ref is not prefixed by '#/components/schemas/'
+    t.equal(openapiObject.components.schemas['def-1'].properties.products.items.$ref, '#/components/schemas/OrderItem')
+
+    // open api validation failed
+    // ENOENT: no such file or directory ${cwd}/OrderItem
     Swagger.validate(openapiObject)
       .then(function (api) {
         t.pass('valid swagger object')
