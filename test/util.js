1dcaa4cf378e8484733fba5a7ce801a3aa72a3a2
KaKa <kaka@kakawebsitedemo.com>
2021-12-08 17:59:03 +0800

    test: 100 coverage

--- test/util.js
+++ test/util.js
@@ -12,9 +12,10 @@ const cases = [
   ['/example/:file(^\\d+).png', '/example/{file}.png'],
   ['/example/at/:hour(^\\d{2})h:minute(^\\d{2})m', '/example/at/{hour}h{minute}m'],
   ['/example/at/(^\\d{2})h(^\\d{2})m', '/example/at/{regexp1}h{regexp2}m'],
+  ['/example/at/(^([0-9]{2})h$)-(^([0-9]{2})m$)','/example/at/{regexp1}-{regexp2}'],
   ['/name::verb', '/name:verb'],
   ['/api/v1/postalcode-jp/:code(^[0-9]{7}$)', '/api/v1/postalcode-jp/{code}'],
-  ['/api/v1/postalcode-jp/(^[0-9]{7}$)', '/api/v1/postalcode-jp/{regexp1}']
+  ['/api/v1/postalcode-jp/(^[0-9]{7}$)', '/api/v1/postalcode-jp/{regexp1}'],
 ]
 
 test('formatParamUrl', t => {
