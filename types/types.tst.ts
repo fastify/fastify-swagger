74d98db405fdf6b3fe93591570e3bf2f33c056e7
Antonio Tripodi <Tony133@users.noreply.github.com>
2026-09-21 21:25:40 +0200

    feat: OpenAPI 3.2.0 compatibility (#949)

--- types/types.tst.ts
+++ types/types.tst.ts
@@ -2,6 +2,8 @@ import fastify, { FastifySchema, RouteOptions } from 'fastify'
 import fastifySwagger, {
   formatParamUrl,
   SwaggerOptions,
+  // eslint-disable-next-line camelcase
+  OpenAPIV3_2,
 } from '..'
 import { minimalOpenApiV3Document } from './minimal-openapiV3-document'
 import { expect } from 'tstyche'
@@ -177,6 +179,42 @@ app
   .ready(() => {
     app.swagger()
   })
+
+// OpenAPI 3.2.0: $self and the extended tag object
+app.register(fastifySwagger, {
+  openapi: {
+    openapi: '3.2.0',
+    $self: 'https://example.com/openapi.json',
+    info: { title: 'Test openapi 3.2', version: '1.0.0' },
+    servers: [{ url: 'https://example.com', name: 'production' }],
+    tags: [
+      { name: 'products', summary: 'Products', kind: 'nav' },
+      { name: 'books', summary: 'Books', parent: 'products', kind: 'nav' }
+    ]
+  },
+  transformObject: (documentObject) => {
+    if ('openapiObject' in documentObject) {
+      // eslint-disable-next-line camelcase
+      expect(documentObject.openapiObject).type.toBe<Partial<OpenAPIV3.Document | OpenAPIV3_1.Document | OpenAPIV3_2.Document>>()
+      return documentObject.openapiObject
+    }
+    return documentObject.swaggerObject
+  }
+})
+
+// eslint-disable-next-line camelcase
+expect<OpenAPIV3_2.PathItemObject>().type.toBeAssignableFrom({
+  query: { responses: {} },
+  additionalOperations: { PROPFIND: { responses: {} } }
+})
+
+// eslint-disable-next-line camelcase
+expect<OpenAPIV3_2.TagObject>().type.toBeAssignableFrom({
+  name: 'books',
+  summary: 'Books',
+  parent: 'products',
+  kind: 'nav'
+})
 
 app.register(fastifySwagger, {
   openapi: {
